6a2b58a3bedbf2e992223c35d6f2929936d1c9f3
Lucas Loureiro <lucas.loureiro@ich.ufjf.br>
2020-07-18 22:22:59 -0300

    Search Twitter

--- Auxilio Emergencial - script.R
+++ Auxilio Emergencial - script.R
@@ -1,1 +1,38 @@
 # codigo para artigo sobre auxilio emergencial 
+
+#Search Twitter 
+
+library(twitteR)
+library(wordcloud)
+library(SnowballC)
+library(tm)
+source('config_twitter_access.R')
+
+setup_twitter_oauth(consumer_key,
+                    consumer_secret_key,
+                    access_token,
+                    access_token_secret)
+
+tweets <- searchTwitter("#caixatemnaofunciona",n = 1000, lang = "pt")
+
+text <- sapply(tweets, function(x) x$getText())
+
+corpus <- Corpus(VectorSource(text))  
+
+dados.texto <- TermDocumentMatrix(corpus,
+                                  control = list(removePunctuation = TRUE,
+                                                 removeNumbers = TRUE, tolower = TRUE))
+
+
+dados.matrix <- as.matrix(dados.texto)
+
+dados.freq <- sort(rowSums(dados.matrix), decreasing = TRUE)
+
+dados.df <- data.frame(term = names(dados.freq) , freq = dados.freq)
+
+library(tidyverse)
+
+
+wordcloud(dados.df$term, dados.df$freq,  min.freq = 5, max.words = 200,random.order = F, colors = brewer.pal(8, 'Dark2'))
+
+dados1 <- dados.df %>% filter(dados.df$freq > 100)
---
+++ config_twitter_access.R
@@ -0,0 +1,7 @@
+# Chaves para acesso a API do Twitter
+consumer_key <- "f0IrGLSVJgsOi84uZl4sFc08e"
+consumer_secret_key <- "E6rI5T4GMY2HWBNFNyNMVN0ThDDln44yKMPdYkgctJXDdlLhgo"
+access_token <- "1273593490304380933-hHSEujsGVeW33KD4InuAYG44wGR5T5"
+access_token_secret <- "8qQx4xPt7JTyLvToeIj98t1oD845yIamw9q6Ub4WdF3l7"
+
+
